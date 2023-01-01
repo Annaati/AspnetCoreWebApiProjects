2dfc231fcab1f25b44337b236b6fca312f26cb0a
Annaati <Abdi17fatah@gmail.com>
2023-01-02 02:38:38 +0300

    initial commit with basic config/set up, root+room(with get) routes

---
+++ LondonAPI/LondonAPI/Controllers/RoomsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LondonAPI.Controllers
+{
+    [Route("/[Controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        [HttpGet(Name = nameof(GetRooms))]
+        public IActionResult GetRooms()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LondonAPI/LondonAPI/Models/ApiErrors.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LondonAPI.Models
+{
+    public class ApiErrors
+    {
+        public String Message { get; set; }
+        public String Detail { get; set; }
+    }
+}
