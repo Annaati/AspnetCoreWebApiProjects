cfec262200da2e610517d534b9ee314d083f6c18
Annaati <Abdi17fatah@gmail.com>
2023-01-08 02:04:52 +0300

    Mogadishu-API: Added Room Model

---
+++ MogadishuAPI/MogadishuAPI/Models/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MogadishuAPI.Models
+{
+    public class Room : Resource
+    {
+        public string Name { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
