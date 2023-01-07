e7a2323d85242e3ee5fe4ff655bed402de0a31ff
Annaati <Abdi17fatah@gmail.com>
2023-01-08 02:10:49 +0300

    Mogadishu-API: Added Services/IRoomService for Room Interface

---
+++ MogadishuAPI/MogadishuAPI/Services/IRoomService.cs
@@ -0,0 +1,15 @@
+﻿using MogadishuAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MogadishuAPI.Services
+{
+    public interface IRoomService
+    {
+        Task<IEnumerable<Room>> GetRoomsAsync();
+
+        Task<Room> getRoomAsync(Guid id);
+    }
+}
