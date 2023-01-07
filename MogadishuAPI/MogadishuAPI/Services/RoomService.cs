99a6bb6598fd433eb1167af60be88c490c4e6286
Annaati <Abdi17fatah@gmail.com>
2023-01-08 02:11:18 +0300

    Mogadishu-API: Added Services/IRoomService for Room Interface Implementation

---
+++ MogadishuAPI/MogadishuAPI/Services/RoomService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
+using MogadishuAPI.Models;
+
+namespace MogadishuAPI.Services
+{
+    public class RoomService : IRoomService
+    {
+        private readonly HotelAPIDbContext _context;
+        private readonly IConfigurationProvider _mappingConfiguration;
+
+        public  RoomService(
+                              HotelAPIDbContext context, 
+                              IConfigurationProvider mappingConfiguration
+                           )
+        {
+            _context = context;
+            _mappingConfiguration = mappingConfiguration;
+        }
+
+        public async Task<Room> getRoomAsync(Guid id)
+        {
+            var entity = await _context.Rooms.SingleOrDefaultAsync(x => x.Id == id);
+
+            if(entity == null)
+            {
+                return null;
+            }
+            var mapper = _mappingConfiguration.CreateMapper();
+            return mapper.Map<Room>(entity);
+        }
+
+        public async Task<IEnumerable<Room>> GetRoomsAsync()
+        {
+            var query = _context.Rooms.ProjectTo<Room>(_mappingConfiguration);
+
+            return await query.ToArrayAsync();
+        }
+
+    }
+}
