fd05224a926e3d99bfccc73d098987aa81885a22
Annaati <Abdi17fatah@gmail.com>
2023-01-08 02:16:27 +0300

    Mogadishu-API: Modified Rooms Controller to reflect Link Resource rewritting

--- MogadishuAPI/MogadishuAPI/Controllers/RoomsController.cs
+++ MogadishuAPI/MogadishuAPI/Controllers/RoomsController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MogadishuAPI.Models;
+using MogadishuAPI.Services;
 
 namespace MogadishuAPI.Controllers
 {
@@ -10,11 +13,41 @@ namespace MogadishuAPI.Controllers
     [ApiController]
     public class RoomsController : ControllerBase
     {
-        [HttpGet(Name = nameof(GetRooms))]
-        [ProducesResponseType(200)]
-        public IActionResult GetRooms()
+        private readonly IRoomService _roomService;
+
+        public RoomsController(IRoomService roomService)
         {
-            throw new NotImplementedException();
+            _roomService = roomService;
         }
+
+        [HttpGet(Name = nameof(GetAllRooms))]
+        [ProducesResponseType(200)]
+        public async Task<ActionResult<Collection<Room>>> GetAllRooms()
+        {
+            var rooms = await _roomService.GetRoomsAsync();
+
+            var collection = new Collection<Room>
+            {
+                Self = Link.ToCollection(nameof(GetAllRooms)),
+                Value = rooms.ToArray()
+            };
+
+            return collection;
+        }
+
+
+        [HttpGet("{roomId}", Name = nameof(GetRoomById))]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(200)]
+        public async Task<ActionResult<Room>> GetRoomById(Guid roomId)
+        {
+            var room = await _roomService.getRoomAsync(roomId);
+            if (room == null) return NotFound();
+
+            return room;
+        }
+
+
+
     }
 }
