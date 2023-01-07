1bf044b797c44de3b5228503fa075d6536555202
Annaati <Abdi17fatah@gmail.com>
2023-01-08 02:12:02 +0300

    Mogadishu-API: Added Room Intity Model

---
+++ MogadishuAPI/MogadishuAPI/Models/RoomEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MogadishuAPI.Models
+{
+    public class RoomEntity
+    {
+        public Guid Id { get; set; }
+        public String Name { get; set; }
+        public int Rate { get; set; }
+    }
+}
