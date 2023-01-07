0fd086bd18e9a01603e37f00f9788e4717009b49
Annaati <Abdi17fatah@gmail.com>
2023-01-08 02:17:08 +0300

    Mogadishu-API: Other Addition & Modifications

---
+++ MogadishuAPI/MogadishuAPI/HotelAPIDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MogadishuAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MogadishuAPI
+{
+    public class HotelAPIDbContext : DbContext
+    {
+        public HotelAPIDbContext(DbContextOptions options) :base(options)
+        {
+
+        }
+
+        public DbSet<RoomEntity> Rooms { get; set; }
+    }
+}
