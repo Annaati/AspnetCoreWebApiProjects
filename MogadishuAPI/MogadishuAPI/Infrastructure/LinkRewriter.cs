a3e6c743d4b3009e8041af028e19e2a51e1f3535
Annaati <Abdi17fatah@gmail.com>
2023-01-08 02:09:45 +0300

    Mogadishu-API: Added Infrustructure/LinkRewriter for Re-writting Resource

---
+++ MogadishuAPI/MogadishuAPI/Infrastructure/LinkRewriter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MogadishuAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MogadishuAPI.Infrastructure
+{
+    public class LinkRewriter
+    {
+        private readonly IUrlHelper _urlHelper;
+
+        public LinkRewriter(IUrlHelper urlHelper)
+        {
+            _urlHelper = urlHelper;
+        }
+
+        public Link Rewrite(Link original)
+        {
+            if (original == null) return null;
+
+            return new Link
+            {
+                Href = _urlHelper.Link(original.RouteName, original.RouteValues),
+                Method = original.Method,
+                Relations = original.Relations
+            };
+        }
+    }
+}
