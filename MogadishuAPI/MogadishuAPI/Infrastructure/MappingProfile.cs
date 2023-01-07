281cc07428a43842b86ba7e72fe79cca9cb2b6cb
Annaati <Abdi17fatah@gmail.com>
2023-01-08 02:08:57 +0300

    Mogadishu-API: Added Infrustructure/Mapping Profile For Model Mapping

---
+++ MogadishuAPI/MogadishuAPI/Infrastructure/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using MogadishuAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MogadishuAPI.Infrastructure
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<RoomEntity, Room>()
+                .ForMember(dest => dest.Rate, option => option.MapFrom(src => src.Rate / 100.0m))
+                .ForMember(dest => dest.Self, opt => opt.MapFrom(src => Link.To(
+                      nameof(Controllers.RoomsController.GetRoomById), new { roomId = src.Id })));
+        }
+    }
+}
