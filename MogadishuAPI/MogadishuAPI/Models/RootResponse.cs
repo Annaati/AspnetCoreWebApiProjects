5631475436717b573b80702988a493196434923a
Annaati <Abdi17fatah@gmail.com>
2023-01-08 02:03:22 +0300

    Mogadishu-API: Added Rot Response Class

---
+++ MogadishuAPI/MogadishuAPI/Models/RootResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MogadishuAPI.Models
+{
+    public class RootResponse : Resource
+    {
+        public Link Rooms { get; set; }
+        public Link HotelInfo { get; set; }
+    }
+}
