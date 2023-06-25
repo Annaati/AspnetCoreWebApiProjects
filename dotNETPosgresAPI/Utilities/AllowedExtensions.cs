ecc614f26a1227f3ec500927dfa4857ee1621e47
Annaati <Abdi17fatah@gmail.com>
2023-06-25 14:05:41 +0300

    Utilities : Added/Implemeted AllowedExtensions Utilitty Class/Services

---
+++ dotNETPosgresAPI/Utilities/AllowedExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotNETPosgresAPI.Utilities
+{
+    public class AllowedExtensions : ValidationAttribute
+    {
+
+        private readonly string[] _extensions;
+        public AllowedExtensions(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+
+        protected override ValidationResult IsValid(
+        object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                var extension = Path.GetExtension(file.FileName);
+                if (!_extensions.Contains(extension.ToLower()))
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+
+        public string GetErrorMessage()
+        {
+            return $"This photo extension is not allowed!";
+        }
+
+
+
+
+
+    }
+}
