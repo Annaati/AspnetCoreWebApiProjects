c0ee6a631e22b444a497262994f51947f2ce70fa
Annaati <Abdi17fatah@gmail.com>
2023-06-25 15:57:55 +0300

    Data : Added Databse Context Class

---
+++ dotNETPosgresAPI/Data/dotNetPostgresDbContext.cs
@@ -0,0 +1,34 @@
+﻿using dotNETPosgresAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace dotNETPosgresAPI.Data
+{
+    public class dotNetPostgresDbContext : DbContext
+    {
+
+        protected readonly IConfiguration _config;
+
+        public dotNetPostgresDbContext(DbContextOptions<dotNetPostgresDbContext> options) : base(options)
+        {
+        }
+
+
+        public DbSet<Users> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql(_config.GetConnectionString("POSTGRES_CONNSTR"));
+        }
+
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Roles>()
+        //        .HasMany(u => u.Users)
+        //        .WithOne(r => r.Role)
+        //        .OnDelete(DeleteBehavior.Cascade);
+        //}
+
+    }
+}
