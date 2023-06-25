952b8324dd5a5ede31aee74df0a0e77359baabef
Annaati <Abdi17fatah@gmail.com>
2023-06-25 13:54:29 +0300

    Services : Injected IAccountService, FileUploadHelper, AddHttpContextAccessor Services into the Program

--- dotNETPosgresAPI/Program.cs
+++ dotNETPosgresAPI/Program.cs
@@ -1,6 +1,17 @@
+using dotNETPosgresAPI.Services.Heplers;
+using dotNETPosgresAPI.Services.Interfaces;
+using dotNETPosgresAPI.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddSingleton<FileUploadHelper>();
+
+
+builder.Services.AddScoped<IAccountService, AccountService>();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
