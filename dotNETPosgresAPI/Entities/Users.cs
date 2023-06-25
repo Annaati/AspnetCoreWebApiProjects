6da5cf3dcfb9ca048e9f9a37164d48fd3ae311ec
Annaati <Abdi17fatah@gmail.com>
2023-06-25 12:29:58 +0300

    Entities : Added User Entities Object

---
+++ dotNETPosgresAPI/Entities/Users.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace dotNETPosgresAPI.Entities
+{
+    /// <summary>
+    /// an Entity Class object representing User Table Structure
+    /// </summary>
+    [Table("tbl_users")]
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Sex { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string ProfImg { get; set; }
+        public int RegBy { get; set;}
+        public string Status { get; set; } = "active";
+        public DateTime RegDate { get; set; } = DateTime.Now;
+    }
+}
