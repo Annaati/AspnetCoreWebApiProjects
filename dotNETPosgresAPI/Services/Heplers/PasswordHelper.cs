3ce3a0f81ac6a7f450166d4b89007d536d84dfc3
Annaati <Abdi17fatah@gmail.com>
2023-06-25 14:02:38 +0300

    Services : Added/Implemented PasswordHelper Service

---
+++ dotNETPosgresAPI/Services/Heplers/PasswordHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace dotNETPosgresAPI.Services.Heplers
+{
+    public class PasswordHelper
+    {
+
+        public static string HashPswd(string password)
+        {
+            byte[] encodedPassword = new UTF8Encoding().GetBytes(password);
+            byte[] hash = ((HashAlgorithm)CryptoConfig.CreateFromName("MD5"))?.ComputeHash(encodedPassword);
+            string encoded = BitConverter.ToString(hash).Replace("-", string.Empty).ToLower();
+
+            return encoded;//returns hashed version of password
+        }
+
+
+
+
+
+    }
+}
