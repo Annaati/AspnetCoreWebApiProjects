f4af926ff06a59c6cc833e993fed4426a8b1d96f
Annaati <Abdi17fatah@gmail.com>
2023-06-25 13:52:14 +0300

    Services : Added a dummy Implementation of IAccountService Interface

---
+++ dotNETPosgresAPI/Services/AccountService.cs
@@ -0,0 +1,65 @@
+﻿using dotNETPosgresAPI.DTO.Account;
+using dotNETPosgresAPI.Services.Interfaces;
+
+namespace dotNETPosgresAPI.Services
+{
+    public class AccountService : IAccountService
+    {
+
+
+
+        #region SignIn Section
+
+        public SigninReq? SignIn(SigninReq req)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+
+        #region User Operations section
+
+        public UserAddReq? AddUser(UserAddReq req)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public UserEditReq? EditUser(UserEditReq req)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+
+        #region User Records section
+
+        public List<UserRes> UsersList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserRes? UserById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+
+        #region Resusbalen Methods section
+
+
+        #endregion
+
+
+
+
+
+    }
+}
