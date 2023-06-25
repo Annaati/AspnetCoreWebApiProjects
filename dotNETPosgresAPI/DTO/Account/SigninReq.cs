86d1d31e9d921b8dac73b94de2d67f64c96edb52
Annaati <Abdi17fatah@gmail.com>
2023-06-25 12:33:22 +0300

    DTO : Added SigninReq DTO Object

---
+++ dotNETPosgresAPI/DTO/Account/SigninReq.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotNETPosgresAPI.DTO.Account
+{
+    public class SigninReq
+    {
+        [Display(Name = "User Id(Email, UserName, Phone)")]
+        [Required(ErrorMessage = "Please Enter One of UserName, Email or Phone Number")]
+        public string? UserId { get; set; }
+
+        [Required(ErrorMessage = "Please Enter your Password")]
+        public string? Password { get; set; }
+    }
+}
