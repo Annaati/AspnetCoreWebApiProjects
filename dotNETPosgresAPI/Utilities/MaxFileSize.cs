38eded046a64c30d4c18a7cdbcdb862612fa0799
Annaati <Abdi17fatah@gmail.com>
2023-06-25 14:06:06 +0300

    Utilities : Added/Implemeted 'MaxFileSize' Utilitty Class/Services

---
+++ dotNETPosgresAPI/Utilities/MaxFileSize.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotNETPosgresAPI.Utilities
+{
+    public class MaxFileSize : ValidationAttribute
+    {
+        private readonly int _maxFileSize;
+        public MaxFileSize(int maxFileSize)
+        {
+            _maxFileSize = maxFileSize;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                if (file.Length > _maxFileSize)
+                {
+                    return new ValidationResult(GetErrorMessage());
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+
+        public string GetErrorMessage()
+        {
+            return $"Maximum allowed file size is {_maxFileSize} bytes.";
+        }
+    
+    
+    
+    
+    
+    }
+}
