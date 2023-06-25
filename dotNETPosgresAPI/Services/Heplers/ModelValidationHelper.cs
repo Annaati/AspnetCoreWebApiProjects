431089e9b7c288cf76e8065d160ed1fc8c4be534
Annaati <Abdi17fatah@gmail.com>
2023-06-25 14:02:19 +0300

    Services : Added/Implemented ModelValidationHelper Services

---
+++ dotNETPosgresAPI/Services/Heplers/ModelValidationHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace dotNETPosgresAPI.Services.Heplers
+{
+    public class ModelValidationHelper
+    {
+
+        internal static void ValidateModel(object obj)
+        {
+            ValidationContext validationContext = new ValidationContext(obj);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+
+            bool IsValid = Validator.TryValidateObject(obj, validationContext, validationResults, true);
+
+            if (!IsValid)
+                throw new ArgumentException(validationResults.FirstOrDefault()?.ErrorMessage);
+        }
+
+
+
+
+
+    }
+}
