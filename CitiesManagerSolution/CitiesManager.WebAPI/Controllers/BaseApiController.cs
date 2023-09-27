bf3ec93f488686b753ed07671c68f96e0232cd82
Annaati <Abdi17fatah@gmail.com>
2023-09-27 06:35:24 +0300

    CitiesManager: initial commit With CitiesManager.WebAPI creation and basic Setup

---
+++ CitiesManagerSolution/CitiesManager.WebAPI/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CitiesManager.WebAPI.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/v{version:ApiVersion}/[controller]/[action]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
---
+++ CitiesManagerSolution/CitiesManager.WebAPI/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddApiVersioning();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "CitiesManager.WebAPI", Version = "v1" });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CitiesManager.WebAPI v1"));
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
+
+
+app.MapControllers();
+
+app.Run();
