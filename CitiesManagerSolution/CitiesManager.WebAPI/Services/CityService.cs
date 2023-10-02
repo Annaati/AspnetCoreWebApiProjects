3ad80ce216af6dacf8491d2ac7f694a674cebaf8
Annaati <Abdi17fatah@gmail.com>
2023-10-02 08:48:19 +0300

    CitiesManager.WebAPI: Added City Interfcae/Service - ongoing implementation

---
+++ CitiesManagerSolution/CitiesManager.WebAPI/Services/CityService.cs
@@ -0,0 +1,91 @@
+﻿using CitiesManager.WebAPI.Data;
+using CitiesManager.WebAPI.DTO.Cities;
+using CitiesManager.WebAPI.DTO.ViewModels;
+using CitiesManager.WebAPI.Entities;
+using CitiesManager.WebAPI.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CitiesManager.WebAPI.Services
+{
+    public class CityService : ICityService
+    {
+
+        #region DI Section
+
+        private readonly MsSqlDbContext _dbContext;
+
+        public CityService(MsSqlDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        #endregion
+
+
+        #region Records Section
+
+        public async Task<ApiResponseVM<IEnumerable<CityRes>>> GetAllCitiesAsync()
+        {
+            var cities = await _dbContext.Cities.Select(c => c.ToCityRes()).ToListAsync();
+
+            return new ApiResponseVM<IEnumerable<CityRes>>(true, 200, "success", "Cities Fetched Successfully", cities);
+        }
+
+
+        
+        public async Task<ApiResponseVM<CityRes>> GetCityByIdAsync(Guid? cityId)
+        {
+            var city = await _dbContext.Cities.Select(c => c.ToCityRes()).Where(c=>c.CityId==cityId).FirstOrDefaultAsync();
+
+            //city = await _dbContext.Cities.Where(c => c.CityId == cityId).FirstOrDefaultAsync();
+            
+
+            return new ApiResponseVM<CityRes>(true, 200, "success", "Cities Fetched Successfully", city);
+        }
+
+        #endregion
+
+
+        #region Actions Section
+
+        public async Task<ApiResponseVM<City>> AddCityAsync(CityAddReq? req)
+        {
+            if(req == null)
+                throw new ArgumentNullException("Provide All required Data", nameof(req));
+
+            var city = req.ToCity();
+
+            await _dbContext.Cities.AddAsync(city);
+            var result = _dbContext.SaveChanges();
+
+            if (result < 1) throw new BadHttpRequestException("Could not Save record", 500);
+
+            return new ApiResponseVM<City>(true, 201, "Success", "City Created Successfuly", city );
+        }
+
+        
+        
+        public Task<ApiResponseVM<City>> UpdateCityAsync(Guid? cityId, CityEditReq? req)
+        {
+            throw new NotImplementedException();
+        }
+        
+        
+        
+        public Task<ApiResponseVM<City>> DeleteCityAsync(Guid? cityId)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+
+
+
+        #region ReUsable Methods Section
+
+        #endregion
+
+
+    }
+}
---
+++ CitiesManagerSolution/CitiesManager.WebAPI/Services/Interfaces/ICityService.cs
@@ -0,0 +1,59 @@
+﻿using CitiesManager.WebAPI.DTO.Cities;
+using CitiesManager.WebAPI.DTO.ViewModels;
+using CitiesManager.WebAPI.Entities;
+
+namespace CitiesManager.WebAPI.Services.Interfaces
+{
+    /// <summary>
+    /// Represents Intreacting/Manipulating City Entity
+    /// </summary>
+    public interface ICityService
+    {
+
+        /// <summary>
+        /// Gets All the existing Cities
+        /// </summary>
+        /// <returns>All Cities as ApiResponseVM of CityRes Object List </returns>
+        Task<ApiResponseVM<IEnumerable<CityRes>>> GetAllCitiesAsync();
+
+
+
+        /// <summary>
+        /// Gets City By City Id
+        /// </summary>
+        /// <param name="cityId">The City id to be return</param>
+        /// <returns>The City that matches the specified City Id as APiResponseVm of Of CityRes Object</returns>
+        Task<ApiResponseVM<CityRes>> GetCityByIdAsync(Guid? cityId);
+
+
+
+        /// <summary>
+        /// Adds a new City to the existing Cities
+        /// </summary>
+        /// <param name="req">the new City to be added</param>
+        /// <returns>APiResponseVM of CityAddReq containing true/false, StatusCode, Title, Message and CityRes Object</returns>
+        Task<ApiResponseVM<City>> AddCityAsync(CityAddReq? req);
+
+
+
+        /// <summary>
+        /// Updates a City
+        /// </summary>
+        /// <param name="cityId">The Id of the City to be updated</param>
+        /// <param name="req">The new City Object to Update to</param>
+        /// <returns>APiResponseVM of CityAddReq containing true/false, StatusCode, Title, Message and CityRes Object</returns>
+        Task<ApiResponseVM<City>> UpdateCityAsync(Guid? cityId, CityEditReq? req);
+
+
+
+        /// <summary>
+        /// Deletes a City 
+        /// </summary>
+        /// <param name="cityId">The Id of the City to be deleted</param>
+        /// <returns>APiResponseVM of CityAddReq containing true/false, StatusCode, Title, Message and Null Data</returns>
+        Task<ApiResponseVM<City>> DeleteCityAsync(Guid? cityId);
+
+
+
+    }
+}
